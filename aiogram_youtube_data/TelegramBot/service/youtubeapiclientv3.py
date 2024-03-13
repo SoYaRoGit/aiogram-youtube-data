e6c7bfa13f19ec754f1e4e7ae7131ad2bb525c91
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-13 18:32:35 +0300

    Начало работы с API YouTube

---
+++ aiogram_youtube_data/TelegramBot/service/youtubeapiclientv3.py
@@ -0,0 +1,22 @@
+from googleapiclient.discovery import build
+from isodate import parse_duration, parse_datetime
+from config.config import load_config_service_youtube
+from urllib.parse import urlparse, parse_qs
+from pathlib import Path
+from utils.logger import logger
+
+
+class YouTubeAPIClientV3:
+    def __init__(self) -> None:
+        try:
+            config = load_config_service_youtube()
+            self.__api_resource = build(
+                serviceName='youtube',
+                version='v3',
+                developerKey=config.service_youtube.api_key_service_youtube_v3
+            )
+        except Exception as e:
+            logger.error(f'An exception occurred while creating the YouTube API resource. Check API_KEY_SERVICE_YOUTUBE: {e}')
+            print(f'An exception occurred while creating the YouTube API resource. Check API_KEY_SERVICE_YOUTUBE: {e}')
+            
+        logger.info('YouTubeAPIClientV3 service launched successfully')
