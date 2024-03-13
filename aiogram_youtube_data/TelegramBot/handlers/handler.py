c78c42285468c8d8f8b72cf0b25cee7d4774e4e4
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-13 20:20:16 +0300

    Изменил lexicon, явно указал какие данные будем получать

--- aiogram_youtube_data/TelegramBot/handlers/handler.py
+++ aiogram_youtube_data/TelegramBot/handlers/handler.py
@@ -1,4 +1,5 @@
-from aiogram import Router
+import re
+from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart, CommandObject, Command
 from lexicon.lexicon_ru import LEXICON_RU
@@ -6,12 +7,44 @@ from lexicon.lexicon_ru import LEXICON_RU
 
 handler_router = Router()
 
-
+# Description Handlers
 @handler_router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.delete()
     await message.answer(
         text=LEXICON_RU['cmd_start'].format(message.from_user.username)
+    )
+
+
+@handler_router.message(F.text == '/help video')
+async def cmd_help_video(message: Message):
+    await message.delete()
+    await message.answer(
+        text=LEXICON_RU['cmd_help_video']
+    )
+
+
+@handler_router.message(F.text == '/help playlist')
+async def cmd_help_playlist(message: Message):
+    await message.delete()
+    await message.answer(
+        text=LEXICON_RU['cmd_help_playlist']
+    )
+
+
+@handler_router.message(F.text == '/help channel')
+async def cmd_help_channel(message: Message):
+    await message.delete()
+    await message.answer(
+        text=LEXICON_RU['cmd_help_channel']
+    )
+
+
+@handler_router.message(F.text == '/help export')
+async def cmd_help_export(message: Message):
+    await message.delete()
+    await message.answer(
+        text=LEXICON_RU['cmd_help_export']
     )
 
 
@@ -21,8 +54,9 @@ async def cmd_help(message: Message):
     await message.answer(
         text=LEXICON_RU['cmd_help']
     )
-    
 
+
+# Handlers for receiving data
 @handler_router.message(Command('video'))
 async def cmd_video(message: Message, command: CommandObject):
     ...
