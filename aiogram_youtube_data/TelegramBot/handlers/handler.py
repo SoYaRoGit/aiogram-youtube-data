db7a27758c7f474212e59924c73c05a964a35b25
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-13 11:13:38 +0300

    Реализация команды /start
    Реализация стартового сообщения командой /start

--- aiogram_youtube_data/TelegramBot/handlers/handler.py
+++ aiogram_youtube_data/TelegramBot/handlers/handler.py
@@ -1,13 +1,15 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart, CommandObject, Command
+from lexicon.lexicon_ru import LEXICON_RU
 
 
 handler_router = Router()
 
 
-@handler_router.message()
-async def msg_echo(msg: Message):
-    await msg.answer(
-        text=f'{msg.text}'
+@handler_router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.delete()
+    await message.answer(
+        text=LEXICON_RU['cmd_start'].format(message.from_user.username)
     )
